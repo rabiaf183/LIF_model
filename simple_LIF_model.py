0c97af212f2c52cc0753fd3c9ef2c83e92552a52
Rabia Fatima <rabiaf183@gmail.com>
2025-11-03 14:26:07 +0100

    Neuron model

---
+++ simple_LIF_model.py
@@ -0,0 +1,54 @@
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+# @markdown Execute this code to initialize the default parameters
+
+def default_pars(**kwargs):
+  pars = {}
+
+  # typical neuron parameters#
+  pars['V_th'] = -55.     # spike threshold [mV]
+  pars['V_reset'] = -75.  # reset potential [mV]
+  pars['tau_m'] = 10.     # membrane time constant [ms]
+  pars['g_L'] = 10.       # leak conductance [nS]
+  pars['V_init'] = -75.   # initial potential [mV]
+  pars['E_L'] = -75.      # leak reversal potential [mV]
+  pars['tref'] = 2.       # refractory time (ms)
+
+  # simulation parameters #
+  pars['T'] = 400.  # Total duration of simulation [ms]
+  pars['dt'] = .1   # Simulation time step [ms]
+
+  # external parameters if any #
+  for k in kwargs:
+    pars[k] = kwargs[k]
+
+  pars['range_t'] = np.arange(0, pars['T'], pars['dt'])  # Vector of discretized time points [ms]
+
+  return pars
+
+
+pars = default_pars()
+print(pars)
+def run_LIF(pars, Iinj, stop=False):
+  """
+  Simulate the LIF dynamics with external input current
+
+  Args:
+    pars       : parameter dictionary
+    Iinj       : input current [pA]. The injected current here can be a value
+                 or an array
+    stop       : boolean. If True, use a current pulse
+
+  Returns:
+    rec_v      : membrane potential
+    rec_sp     : spike times
+  """
+
+  # Set parameters
+  V_th, V_reset = pars['V_th'], pars['V_reset']
+  tau_m, g_L = pars['tau_m'], pars['g_L']
+  V_init, E_L = pars['V_init'], pars['E_L']
+  dt, range_t = pars['dt'], pars['range_t']
+  Lt = range_t.size
+  tref = pars['tref']
